29f1f5b4b4ccdac1880a61fd0f85cbf1d4011d0b
Daniel <dclark205@gmail.com>
2017-07-13 18:17:18 -0400

    Add files via upload

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main ( String[] args ) {
+		System.out.println( "I, Daniel Clark, am determined to learn how to code." );
+		System.out.println( "Today's date is Thursday, July 13, 2017");
+	}
+}
